6b1c724807af355fa0257da56b41a4785a5d1c88
AnyaVasetskaya <annav27@nycstudents.net>
2022-09-26 14:07:30 -0400

    done with lab

--- src/Cat.java
+++ src/Cat.java
@@ -1,2 +1,33 @@
+// Cat class definition
 public class Cat {
+
+    // instance variables
+    private String name;
+    private int age;
+    private double weight;
+
+    private String youngOrOld;
+
+    // constructor
+    public Cat(String catName, int catAge, double catWeight) {
+        name = catName;
+        age = catAge;
+        weight = catWeight;
+        if (catAge < 7)
+            youngOrOld = "a younger";
+        else
+            youngOrOld = "an older";
+    }
+
+    // method that introduces the Cat
+    public void introduce() {
+        System.out.println("Hello my name is " + name + " and I am " + youngOrOld + " cat");
+    }
+
+    // method that prints Cat info
+    public void printCatInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Weight: " + weight);
+    }
 }
--- src/CatRunner.java
+++ src/CatRunner.java
@@ -1,2 +1,11 @@
 public class CatRunner {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat("Killua", 6, 4.5);
+        cat1.introduce();
+        cat1.printCatInfo();
+
+        Cat cat2 = new Cat("Jiji", 13, 6.3);
+        cat2.introduce();
+        cat2.printCatInfo();
+    }
 }
